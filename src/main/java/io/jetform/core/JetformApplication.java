0d87505a30d1c0c7000ff29d444dba19f2e5bce6
Deepak <1234.dsp.dsp@gmail.com>
2021-10-06 11:41:02 +0530

    initial commit

---
+++ src/main/java/io/jetform/core/JetformApplication.java
@@ -0,0 +1,12 @@
+package io.jetform.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetformApplication.class, args);
+	}
+}
---
+++ src/test/java/io/jetform/core/JetformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jetform.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
