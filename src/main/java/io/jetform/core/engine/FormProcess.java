a5bdeb3e61f22fb482b69a63eb0f7ec471fbb347
Rahul Baghel <rahulbaghel0751@gmail.com>
2021-10-06 13:06:28 +0530

    changes in sysout

--- src/main/java/io/jetform/core/engine/FormProcess.java
+++ src/main/java/io/jetform/core/engine/FormProcess.java
@@ -3,6 +3,6 @@ package io.jetform.core.engine;
 public class FormProcess {
    public static void main(String[] args) {
 	System.out.println("heell");
-	System.out.println();
+	System.out.println("form process");
 }
 }
